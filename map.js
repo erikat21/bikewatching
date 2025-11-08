6acd09d720f686a7fd8a9386652247cd214c3e21
erikat21 <ertong@ucsd.edu>
2025-11-08 13:14:33 -0800

    step 1 done

---
+++ map.js
@@ -0,0 +1,12 @@
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXJ0b25nMjEiLCJhIjoiY21ocXJkeTFkMTFlczJsb2hmczMwZHZlZiJ9.qwOHyZ8MSG4kaN68ifsoaw';
+
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
